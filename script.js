7135d7f582e85822a0e309e0c3e5b90d7999c127
falak-falak <falakshairwin@gmail.com>
2025-02-21 13:25:18 +0500

    initial commit

---
+++ script.js
@@ -0,0 +1,43 @@
+// NAV BAR SCROLLING EFFECT
+window.addEventListener("scroll", () => {
+  const navBar = document.querySelector(".nav-bar");
+  const logo = document.querySelector(".logo-text");
+  const scrollThreshold = 50;
+  if (window.scrollY > scrollThreshold) {
+    navBar.classList.add("nav-bar-scrolled");
+    logo.classList.add("logo-scrolled");
+  } else {
+    navBar.classList.remove("nav-bar-scrolled");
+    logo.classList.remove("logo-scrolled");
+  }
+});
+
+// COUNTER ANIMATIONS
+const counts = document.querySelectorAll(".count");
+// const speed = 90;
+counts.forEach((counter) => {
+  function upData() {
+    const speed = Number(counter.getAttribute("data-speed"));
+    const target = Number(counter.getAttribute("data-target"));
+    const count = counts.innerText;
+    const inc = target / speed;
+    if (count < target) {
+      counter.innerText = inc + count;
+      setTimeout(upData, 20);
+    } else {
+      counter.innerText = target;
+    };
+  };
+  upData();
+});
+
+//SKILLS ANIMATION
+document.addEventListener("DOMContentLoaded", () => {
+  const progressBar = document.querySelectorAll(".progress");
+  progressBar.forEach((bar) => {
+    const percent = bar.getAttribute("data-percent");
+    bar.style.width = percent + "%" ;
+    bar.innerText = percent + "%";
+  });
+});
+
